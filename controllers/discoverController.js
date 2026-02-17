1a5fdbb45a660c974c4ceba47fbf2614865be210
bilal <bilal@gmail.com>
2026-02-17 16:15:48 +0530

    discover vontroller updated

--- controllers/discoverController.js
+++ controllers/discoverController.js
@@ -4,72 +4,17 @@ import { StatusCodes } from "http-status-codes";
 import { Album } from "../models/album.model.js";
 import { shapeAlbumResponse } from "../dto/album.dto.js";
 import { shapeSongResponse } from "../dto/song.dto.js";
+import { getRandomArtistWithSongsService } from "../services/random.service.js";
 
 
 export const getRandomArtistWithSongs = async (req, res) => {
-  const totalArtists = await Artist.countDocuments();
-  if (totalArtists === 0) {
-    return res.status(StatusCodes.NOT_FOUND).json({
-      success: false,
-      message: "No artists found"
-    });
-  }
+  const result = await getRandomArtistWithSongsService(req.query);
 
-  // Pick a random artist
-  const randomIndex = Math.floor(Math.random() * totalArtists);
-  const artist = await Artist.findOne({isMonetizationComplete: true}).skip(randomIndex).lean();
-
-  if (!artist) {
-    return res.status(StatusCodes.NOT_FOUND).json({
-      success: false,
-      message: "Random artist not found"
-    });
-  }
-
-  // Pagination params
-  const page = Math.max(1, parseInt(req.query.page, 10) || 1);
-  const limit = Math.min(50, Math.max(1, parseInt(req.query.limit, 10) || 10));
-  const skip = (page - 1) * limit;
-
-  // Get total songs by the artist
-  const totalSongs = await Song.countDocuments({ artist: artist._id });
-
-  // Get paginated songs
-  const songs = await Song.find({ artist: artist._id })
-    .select("_id title coverImageKey duration")
-    .sort({ createdAt: -1 })
-    .skip(skip)
-    .limit(limit);
-
-      const shapedSongs = await Promise.all(
-        songs.map(async (song) => {
-          
-  
-          return shapeSongResponse(song);
-        })
-      );
-
-  
-
-  res.status(StatusCodes.OK).json({
+  return res.status(StatusCodes.OK).json({
     success: true,
-    artist: {
-      _id: artist._id,
-      name: artist.name,
-      slug: artist.slug,
-      profileImage: artist.profileImage,
-    
-    },
-    songs:shapedSongs,
-    pagination: {
-      total: totalSongs,
-      page,
-      limit,
-      totalPages: Math.ceil(totalSongs / limit)
-    }
+    ...result,
   });
 };
-
 
 export const getExploreFeed = async (req, res) => {
   try {
