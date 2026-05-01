30a381ed4ded86964c0c84fe6ffc2786ade0134b
bilal <bilal@gmail.com>
2026-05-01 15:24:31 +0530

    resolving redis issue

--- utils/redisClient.js
+++ utils/redisClient.js
@@ -5,6 +5,8 @@ import Redis from "ioredis";
 const redis = new Redis({
   host: process.env.REDIS_HOST || "127.0.0.1", // fallback
   port: process.env.REDIS_PORT || 6379,
+}, {
+  maxRetriesPerRequest: null,
 });
 
 // Optional: log connection events
